fd26a9267e08d3b4e17a96cab07ddcb0eb7dfc65
Achilles P. Loumis <achilleas.loumis@gmail.com>
2018-11-11 22:41:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
- expect(shout(string)) {
+ function shout(string) {
   return string.toUpperCase()
 }
 
-function  whisper(string) {
+function whisper(string) {
   return string.toLowerCase()
 }
 
 function SayHiToGrandma(string) {
-  if string is all lowercase return "I can't hear you!"
+  if else return "I can't hear you!"
 }
